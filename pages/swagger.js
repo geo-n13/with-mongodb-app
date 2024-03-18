574f862602f74c0dbf4a05463af2842257190c02
Geo Mercore <geomercorepro@outlook.fr>
2024-03-18 08:07:32 +0100

    swagger de nouveau fonctionnel après rectifications

--- pages/swagger.js
+++ pages/swagger.js
@@ -1,11 +1,13 @@
 // pages/swagger.js
-import SwaggerUI from 'swagger-ui-react';
+import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react').then(module => module.default), { ssr: false });
 
 const SwaggerPage = () => {
     return (
         <SwaggerUI url="/api/swagger" />
     );
-}
+};
 
 export default SwaggerPage;
